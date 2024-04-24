6e1324eb2ee3d0acef29c6c6b83f4ce6ac1a6348
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-24 13:21:35 -0300

    working on content view

--- 3d_content/ContentView.swift
+++ 3d_content/ContentView.swift
@@ -14,6 +14,15 @@ struct ContentView: View {
                 .foregroundColor(.secondary)
                 .frame(height: 500)
                 .ignoresSafeArea()
+            VStack(spacing: 12) {
+                Text("Start Your Brand")
+                    .font(.title.bold())
+                
+                Text("Stay up to date with all your social media platform in one simple app!")
+                    .foregroundStyle(.secondary)
+                
+            }
+            .padding(.horizontal)
         }
     }
 }
